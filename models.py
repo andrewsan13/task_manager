4be6fee03526758b8b5850f2d107153f63cecb61
andrewsan13 <andrewsan12@gmail.com>
2020-05-09 12:52:01 +0300

    change models for class Task, Project, Calendar

--- models.py
+++ models.py
@@ -1,12 +1,39 @@
 class Project:
-    def __init__(self, p_id, name='None'):
+    def __init__(self, name):
         self.name: str = name
-        self.p_id: int = p_id
+        self.tasks = []
 
-    class Task:
-        def __init__(self, t_id, project_id, name='Nothing', status='Closed', priority='Regular'):
-            self.t_id: int = t_id
-            self.name: str = name
-            self.status: str = status
-            self.project_id: int = project_id
-            self.priority: str = priority
+    def showProjectName(self):
+        return self.name
+
+    def showProjectTasks(self):
+        return self.tasks
+
+    def newTask(self, name, status, priority):
+        new = Task(name, status, priority)
+        self.tasks.append(new)
+        return new
+
+
+class Calendar:
+    def __init__(self, date):
+        self.date = date
+        self.tasks = []
+
+    def showCalendarDate(self):
+        return self.date
+
+    def showCalendarTasks(self):
+        return self.tasks
+
+    def newTask(self, name, status, priority):
+        new = Task(name, status, priority)
+        self.tasks.append(new)
+        return new
+
+
+class Task:
+    def __init__(self, name, status, priority):
+        self.name: str = name
+        self.status: str = status
+        self.priority: str = priority
