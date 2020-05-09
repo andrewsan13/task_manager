bec018a5f05222dd013bc1cf527dd11c314900ae
andrewsan13 <andrewsan12@gmail.com>
2020-05-09 15:22:54 +0300

    create Task method in application

--- application.py
+++ application.py
@@ -2,6 +2,8 @@ from PyQt5 import QtWidgets
 from PyQt5.QtCore import QDate
 
 import design  # наш файл дизайна
+from models import *
+import database
 
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
@@ -12,8 +14,6 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.btnSearch.clicked.connect(self.show_tasks)
         # self.calendarWidget.clicked[QDate].connect(self.show_tasks)
         self.btnCreateTask.clicked.connect(self.create_task)
-        self.tasks = {}
-        self.projects = {}
         self.btnDeleteProj.clicked.connect(self.show_projects)
         self.btnCreateProj.clicked.connect(self.create_project)
         self.listWidget_Projects.clicked.connect(self.change_action_proj)
@@ -25,6 +25,26 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
     def change_action_proj(self):
         self.last_action = "projects"
+
+    def create_task(self):
+        name, ok1 = QtWidgets.QInputDialog.getText(self, "Name", "Enter a name for the task:")
+        priority, ok2 = QtWidgets.QInputDialog.getItem(self, "Priority", "Choose priority:", Task.getPriority())
+        print(type(priority))
+        print(type(name))
+        if ok1 and ok2 and name and priority:
+            new_task = Task(name, priority)
+            if self.last_action == 'calendar' or not self.last_action:
+                date = self.calendarWidget.selectedDate()
+                database.nctask(date, new_task)
+            else:
+                proj = self.listWidget_Projects.selectedItems()[0].text()
+                database.nptask(proj, new_task)
+        else:
+            w = QtWidgets.QMessageBox()
+            w.setWindowTitle("Error Message")
+            w.setText("Task is not added.")
+            w.exec()
+
 
     def show_tasks(self):
         self.listWidget_Tasks.clear()
@@ -39,15 +59,6 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                 self.listWidget_Tasks.addItem(self.tasks[hash(date)])
             else:
                 self.listWidget_Tasks.addItem(' ')
-
-    def create_task(self):
-        task, ok = QtWidgets.QInputDialog.getText(self, "Create Task", "Enter task:")
-        if self.last_action == 'calendar' or not self.last_action:
-            date = self.calendarWidget.selectedDate()
-            self.tasks[hash(date)] = task
-        else:
-            proj = self.listWidget_Projects.selectedItems()[0].text()
-            self.projects[proj][hash(task)] = task
 
     def edit_task(self):
         pass
