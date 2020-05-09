a2be5a0f58e528656c634a303f98927c36b866f4
andrewsan13 <andrewsan12@gmail.com>
2020-05-09 15:21:27 +0300

    change order in listPriority for getItem in new Task

--- models.py
+++ models.py
@@ -9,10 +9,8 @@ class Project:
     def showProjectTasks(self):
         return self.tasks
 
-    def newTask(self, name, status, priority):
-        new = Task(name, status, priority)
-        self.tasks.append(new)
-        return new
+    def addTask(self, task):
+        self.tasks.append(task)
 
 
 class Calendar:
@@ -26,14 +24,32 @@ class Calendar:
     def showCalendarTasks(self):
         return self.tasks
 
-    def newTask(self, name, status, priority):
-        new = Task(name, status, priority)
-        self.tasks.append(new)
-        return new
+    def addTask(self, task):
+        self.tasks.append(task)
 
 
 class Task:
-    def __init__(self, name, status, priority):
+    listPriority = ('Regular', 'Hot', 'Lazy')
+    listStatus = ('In progress', 'Done')
+
+    def __init__(self, name, priority, status='In progress'):
         self.name: str = name
         self.status: str = status
         self.priority: str = priority
+
+    @classmethod
+    def getPriority(cls):
+        return cls.listPriority
+
+    @classmethod
+    def getStatus(cls):
+        return cls.listStatus
+
+    def showTaskName(self):
+        return self.name
+
+    def showTaskStatus(self):
+        return self.status
+
+    def showTaskPriority(self):
+        return self.priority
