1734d506e8931471db29e420f1cf884fe18917e8
andrewsan13 <andrewsan12@gmail.com>
2020-05-15 00:56:36 +0300

    change database from localhost to mongodb cluster

--- database.py
+++ database.py
@@ -3,8 +3,9 @@ import models
 import pymongo
 from bson.objectid import ObjectId
 
-mongoclient = pymongo.MongoClient("mongodb://localhost:27017/")
-# f"mongodb+srv://{USER}:{PASSWORD}@cluster0-idlrk.azure.mongodb.net/test?retryWrites=true&w=majority"
+# mongoclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mongoclient = pymongo.MongoClient(
+    f"mongodb+srv://{USER}:{PASSWORD}@cluster0-idlrk.azure.mongodb.net/test?retryWrites=true&w=majority")
 database = mongoclient["mydatabase"]
 dbproj = database["projects"]
 dbcale = database["calendar"]
@@ -14,6 +15,8 @@ dbcale = database["calendar"]
 def addproject(name, project):  # new project
     new = {'name': name, 'project': project}
     dbproj.insert_one(new)
+
+
 def editproject(last, newname, project):  # update
     old = {"name": last}
     new = {"$set": {'name': newname, "project": project}}
@@ -21,48 +24,71 @@ def editproject(last, newname, project):  # update
     dbproj.update_one(old, new)
     if dbproj[last]:
         del dbproj[last]  # удаляем таски старого имени
+
+
 def deleteproject(name):  # delete
     wbd = {"name": name}
     dbproj.delete_one(wbd)
     if dbproj[name]:
         del dbproj[name]  # Удаляем таски этого проэкта dbproj[name]['tasks']
+
+
 def getprojects():  # find
     return dbproj.find()
+
+
 def findoneproject(name):
     return dbproj.find_one({'name': name})
+
 
 # PROJECT TASKS
 def addptask(proj, taskname, task):  # new
     new = {'name': taskname, 'task': task}
     dbproj[proj]['tasks'].insert_one(new)
+
+
 def editptask(proj, oldname, newname, task):  # update
     old = {"name": oldname}
     new = {"$set": {'name': newname, "task": task}}
     dbproj[proj]['tasks'].update_one(old, new)
+
+
 def deleteptask(proj, name):  # delete
     wbd = {"name": name}
     dbproj[proj]['tasks'].delete_one(wbd)
+
+
 def getptask(proj):  # find
     return dbproj[proj]['tasks'].find()
+
 
 # CALENDAR TASKS
 def addctask(date, name, task):  # new calendar task
     new = {'name': name, 'task': task}
     dbcale[date]['tasks'].insert_one(new)
+
+
 def editctask(date, oldname, newname, task):  # update
     old = {'name': oldname}
     new = {"$set": {'name': newname, 'task': task}}
     dbcale[date].update_one(old, new)
+
+
 def deletectask(date, name):  # delete
     wbd = {'name': name}
     dbcale[date].delete_one(wbd)
+
+
 def getctask(date):  # find
     return dbcale[date]['tasks'].find()
+
 
 # DATABASE
 def _clearDatabase():
     for col in database.collection_names():
         database.drop_collection(col)
+
+
 def _showDatabase():
     for col in database.collection_names():
         print("Collection name :", database[col])
