44443325b84e9d46b75a911e5cd27e0f07ddcb5a
andrewsan13 <andrewsan12@gmail.com>
2020-05-14 22:41:02 +0300

    did what i wanted, It's Done. Add icon for application

--- application.py
+++ application.py
@@ -18,13 +18,18 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
         self.show_projects()  # Показать проэкты, если есть в базе данных
         self.show_tasks()  # Показать задачи, если есть (дефолт на текущую дату)
+        self.current_date()
         self.calendarWidget.clicked[QDate].connect(self.show_tasks)  # показать задачи при нажатии на дату
         self.listWidget_Projects.clicked.connect(self.show_tasks)  # показать задачи при нажатии на проэкт
 
-        # self.btnSearch.clicked.connect(self.search)
-        self.btnDeleteProj.clicked.connect(self.edit_project)
-        self.btnCreateTask.clicked.connect(self.create_task)
         self.btnCreateProj.clicked.connect(self.create_project)
+        self.btnEditProj.clicked.connect(self.edit_project)
+        self.btnDeleteProj.clicked.connect(self.delete_project)
+
+        self.btnCreateTask.clicked.connect(self.create_task)
+        self.btnEditTask.clicked.connect(self.edit_task)
+        self.btnDeleteTask.clicked.connect(self.delete_task)
+        self.btnSearch.clicked.connect(self.search)
 
     def change_action_calendar(self):
         self.last_action = "calendar"
@@ -66,9 +71,36 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                 self.listWidget_Tasks.addItem(one.name)
 
     def edit_task(self):
-        pass
+        oldname = self.listWidget_Tasks.selectedItems()[0].text()
+        name, ok1 = QtWidgets.QInputDialog.getText(self, "Name", f"Current name: {oldname}\nEnter a name for the task:")
+        if ok1 and name:
+            priority, ok2 = QtWidgets.QInputDialog.getItem(self, "Priority", "Choose priority:", Task.listPriority)
+            if ok2 and priority:
+                status, ok3 = QtWidgets.QInputDialog.getItem(self, "Status", "Choose status:", Task.listStatus)
+                if ok3 and status:
+                    if self.last_action == 'projects':
+                        proj = self.listWidget_Projects.selectedItems()[0].text()
+                        convertor.editprojecttask(proj, oldname, name, priority, status)
+                    else:
+                        date = self.calendarWidget.selectedDate()
+                        convertor.editcalendartask(date, oldname, name, priority, status)
+                else:
+                    self.message_error('Task', 'updated')
+            else:
+                self.message_error('Task', 'updated')
+        else:
+            self.message_error('Task', 'updated')
 
     def delete_task(self):
+        task = self.listWidget_Tasks.selectedItems()[0].text()
+        if self.last_action == 'projects':
+            proj = self.listWidget_Projects.selectedItems()[0].text()
+            convertor.deleteprojecttask(proj, task)
+        else:
+            date = self.calendarWidget.selectedDate()
+            convertor.deletecalendartask(date, task)
+
+    def search(self):
         pass
 
     def create_project(self):
@@ -95,4 +127,12 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.show_projects()
 
     def delete_project(self):
-        pass
+        proj = self.listWidget_Projects.selectedItems()[0].text()
+        convertor.deleteproject(proj)
+        self.show_projects()
+
+    def current_date(self):
+        self.calendarWidget.showToday()
+        date = self.calendarWidget.selectedDate()
+        self.labelDate.setText(f"{date.longDayName(date.day())} {date.day()} {date.longMonthName(date.month())}\
+ of {date.year()}")
